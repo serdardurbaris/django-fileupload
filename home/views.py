0c6a5e95a1de7482bc966693e13a81a3c746a8f5
Serdar DURBARIŞ <serdar@sambapos.com>
2018-04-08 23:20:54 +0300

    1

---
+++ home/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'index.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'index.html')
